041cd50b9ae2225880b66612ddd0d4b08d4c51ec
jinfanbo <jinfanbo0320@163.com>
2018-09-13 15:24:16 +0800

    二分查找

---
+++ 二分查找.py
@@ -0,0 +1,26 @@
+def binary_search1(alist, item):
+    if len(alist) == 0:
+        return False
+    else:
+        midpoint = len(alist)//2
+        if alist[midpoint]==item:
+          return True
+        else:
+          if item<alist[midpoint]:
+            return binary_search1(alist[:midpoint],item)
+          else:
+            return binary_search1(alist[midpoint+1:],item)
+
+
+def binary_search2(alist, item):
+    first = 0
+    last = len(alist) - 1
+    while first <= last:
+        midpoint = (first + last) / 2
+        if alist[midpoint] == item:
+            return True
+        elif item < alist[midpoint]:
+            last = midpoint - 1
+        else:
+            first = midpoint + 1
+    return False
