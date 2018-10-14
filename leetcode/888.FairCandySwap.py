e21795388159d8b3763c97128f1e9938ee5aefb1
jinfanbo <jinfanbo0320@163.com>
2018-10-14 22:46:35 +0800

    2018-10-14

--- leetcode/888.FairCandySwap.py
+++ leetcode/888.FairCandySwap.py
@@ -5,3 +5,14 @@ class Solution:
         :type B: List[int]
         :rtype: List[int]
         """
+        a, b, c = sum(A), sum(B), set(B)
+        for member in A:
+            flag = (b - a) // 2 + member
+            if flag in c:
+                return [member, flag]
+if __name__ == '__main__':
+    a = Solution()
+    print(a.fairCandySwap([1, 2], [2, 3]))
+    print(a.fairCandySwap([1, 1], [2, 2]))
+    print(a.fairCandySwap([2], [1, 3]))
+    print(a.fairCandySwap([1, 2, 5], [2, 4]))
